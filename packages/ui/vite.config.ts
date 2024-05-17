0889fe529c0ffc9162b43691bd2a85a54a589296
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-17 15:19:17 +0900

    feat: ui 워크스페이스 vite library 모드 빌드 설정

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: "src/primitive",
+      name: "@warrrui-test/ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      input: {
+        Box: "/src/primitive/Box",
+        Button: "/src/primitive/Button",
+      },
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          dir: "dist",
+        },
+        {
+          format: "cjs",
+          entryFileNames: "[name].cjs",
+          dir: "dist",
+        },
+        {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+      ],
+    },
+  },
+});
