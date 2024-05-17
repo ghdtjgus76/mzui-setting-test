5dc74ac6b0070268d35405af62a2040fdd9c1f74
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-17 11:54:25 +0900

    chore: 패키지명 변경

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["mzui"],
+  transpilePackages: ["warrrui"],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
