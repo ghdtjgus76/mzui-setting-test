caa14fd70fb9d4b6972b48d5c392888b6a9f39d1
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-17 15:32:51 +0900

    chore: vite 빌드 시 타입스크립트 선언 파일 설정 변경

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,7 +4,13 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      aliasesExclude: ["stories.tsx", "stories.ts"],
+      exclude: "src/themed/**/*"
+    }),
+  ],
   build: {
     lib: {
       entry: ["src/primitive/Box", "src/primitive/Button"],
