0660d4b6279cb42f10d8b68be60ea215b2137474
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-23 13:20:22 +0900

    feat: plop을 이용한 svg -> react component 변환 기능 추가

--- packages/codegen/plopfile.js
+++ packages/codegen/plopfile.js
@@ -1,4 +1,7 @@
-export default function (plop) {
+import fs from "fs";
+import path from "path";
+
+export default async function (plop) {
   plop.setGenerator("Story", {
     description: "Create a story file",
     prompts: [
@@ -20,5 +23,43 @@ export default function (plop) {
         templateFile: "templates/Story.tsx.hbs",
       },
     ],
+  });
+
+  plop.setGenerator("Icon", {
+    description: "Convert SVG to React component",
+    prompts: [],
+    actions: () => {
+      try {
+      } catch (e) {
+        console.log(e);
+      }
+      const actions = [];
+      const svgDir = "../icons/src/svg";
+      const svgFiles = fs
+        .readdirSync(svgDir)
+        .filter((file) => file.endsWith(".svg"));
+
+      for (const file of svgFiles) {
+        const componentName = path
+          .basename(file, ".svg")
+          .replace(/(^\w|-\w)/g, (match) =>
+            match.replace("-", "").toUpperCase()
+          );
+        const svgFilePath = path.resolve(svgDir, file);
+        const componentContent = fs.readFileSync(svgFilePath).toString();
+
+        actions.push({
+          type: "add",
+          path: `../icons/src/react/{{pascalCase componentName}}.tsx`,
+          templateFile: "templates/Icon.tsx.hbs",
+          data: {
+            componentName,
+            componentContent,
+          },
+        });
+      }
+
+      return actions;
+    },
   });
 }
--- packages/icons/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as UpArrow } from "./UpArrow.tsx";
-
-export { default as UpArrowIcon } from "./up-arrow.svg";
- 
