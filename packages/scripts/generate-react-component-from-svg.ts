6f8c4f7a33eee5f4c3c7982bccb810ea17662052
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-23 19:35:02 +0900

    feat: svg 파일에서 컴포넌트 방식으로 변환하는 스크립트 작성

--- packages/scripts/generate-react-component-from-svg.ts
+++ packages/scripts/generate-react-component-from-svg.ts
@@ -1,3 +1,39 @@
-const generateReactComponentFromSvg = () => {};
+import { promises as fs } from "fs";
+import path from "path";
+import prettier from "prettier";
+
+const generateReactComponentFromSvg = async () => {
+  const svgDir = "../icons/src/svg";
+  const svgFiles = (await fs.readdir(svgDir)).filter((file) =>
+    file.endsWith(".svg")
+  );
+
+  for (const file of svgFiles) {
+    const componentName = path
+      .basename(file, ".svg")
+      .replace(/(^\w|-\w)/g, (match) => match.replace("-", "").toUpperCase());
+    const svgFilePath = path.resolve(svgDir, file);
+    const svgContent = (await fs.readFile(svgFilePath)).toString();
+
+    const componentContent = `
+      const ${componentName} = () => (
+        ${svgContent}
+      );
+
+      export default ${componentName};
+    `;
+    const componentDir = "../icons/src/react";
+    const componentFilePath = path.resolve(
+      componentDir,
+      `${componentName}.tsx`
+    );
+
+    const formattedComponentContent = await prettier.format(componentContent, {
+      parser: "babel",
+    });
+
+    await fs.writeFile(componentFilePath, formattedComponentContent);
+  }
+};
 
 generateReactComponentFromSvg();
